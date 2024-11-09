2f7df95155feb912402c72fb4060a01882d04433
aahnik <daw@aahnik.dev>
2024-11-09 19:30:17 +0530

    Fix the update score dto, allow only changing score value and not other fields

--- api/src/scores/dto/update-score.dto.ts
+++ api/src/scores/dto/update-score.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateScoreDto } from './create-score.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, Max, Min } from 'class-validator';
 
-export class UpdateScoreDto extends PartialType(CreateScoreDto) {}
+export class UpdateScoreDto {
+  @ApiProperty({ required: true, description: 'Score for the challenge' })
+  @IsInt()
+  @Min(0)
+  @Max(1)
+  score: number;
+}
