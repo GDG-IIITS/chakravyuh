089c8cbef09600061e91e8d3c7d2cb552fe31849
aahnik <daw@aahnik.dev>
2024-11-09 18:06:04 +0530

    Update IUpdateUserDto with user role

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,9 +1,12 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { AdminCreateUserDto, ICreateUserDto } from './create-user.dto';
+import { URoles } from '../users.schema';
 
 export class IUpdateUserDto extends PartialType(ICreateUserDto) {
   emailVerified?: boolean;
   emailConfirmedAt?: Date;
+  @ApiProperty({ enum: URoles })
+  role?: string;
 }
 
 export class AdminUpdateUserDto extends PartialType(AdminCreateUserDto) {}
