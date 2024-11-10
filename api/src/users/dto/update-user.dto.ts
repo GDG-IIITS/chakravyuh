a6effdf0407a22bc9f2da255c3ca3bc7a6b80324
aahnik <daw@aahnik.dev>
2024-11-11 02:11:49 +0530

    Remove email verified related fields from update-user dto

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -3,8 +3,6 @@ import { AdminCreateUserDto, ICreateUserDto } from './create-user.dto';
 import { URoles } from '../users.schema';
 
 export class IUpdateUserDto extends PartialType(ICreateUserDto) {
-  emailVerified?: boolean;
-  emailConfirmedAt?: Date;
   @ApiProperty({ enum: URoles })
   role?: string;
 }
