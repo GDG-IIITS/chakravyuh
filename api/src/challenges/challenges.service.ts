be37871f19546ccf25ca66cd9eb431751b2699e3
aahnik <daw@aahnik.dev>
2024-11-11 18:00:19 +0530

    Fix time logic in canSubmit

--- api/src/challenges/challenges.service.ts
+++ api/src/challenges/challenges.service.ts
@@ -105,13 +105,13 @@ export class ChallengesService {
     if (!challenge) {
       throw new NotFoundException(`Challenge with given not found`);
     }
-    // const now = Date.now();
-    // if (
-    //   now < challenge.startTime.getTime() ||
-    //   now > challenge.endTime.getTime()
-    // ) {
-    //   throw new NotAcceptableException('Challenge is not active');
-    // }
+    const now = Date.now();
+    if (
+      now > challenge.startTime.getTime() ||
+      now < challenge.endTime.getTime()
+    ) {
+      throw new NotAcceptableException('Challenge is not active');
+    }
     if (!team) {
       throw new NotFoundException('Team not found');
     }
